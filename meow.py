82cb7edf9f0b05de438d7ec5490eabb1b9b02dcb
Vasiluk Dmitrij <vasilukwolf@gmail.com>
2014-04-16 00:02:08 +0400

    if else function

---
+++ meow.py
@@ -0,0 +1,14 @@
+#Вызов функции
+def myfunk(Nashebablo):
+#Тело фунуции
+    if Nashebablo >= 100:
+        print("v shokolade")
+    elif Nashebablo <= 100:
+        print ("pizdets")
+    else:
+        print ("tozhe norm")
+
+myfunk(1000000000)
+myfunk(99)
+myfunk(1000000000)
+myfunk(99)
---
+++ wolf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 _*_
+print (("hello world " + str(300)))
+v = 2.001
+w = 3
+print(v)
+print(w)
+print ((type(w)))
+print ((type(v)))
+y = 3
+print((y * y))
+X = 34
+print ((type(X)))
+
+X = 34.1
+print (("X=", X))
+print ((type(X)))
+X = None
+print ((type(X)))
+Z = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print ((type(Z)))
+print (Z)
+C = "hellohardcod"
+print ((C[:-1]))
+print (("20" * 2))
+print ("Привет")
+tv1 = 90
+tv2 = 1
+
+if tv1 + tv2 > 99:
+    print ("Сумма недостаточна")
+else:
+    print ("Чек оплачен")
+print ("Пока")
+vs = 0
+print ((str(vs)))
+a = True
+b = True
+trf = a != b
+print ((trf))
